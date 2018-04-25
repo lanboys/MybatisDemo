394deb2a31e31fdd75cbd2a4235e561a3f55d48b
lanbing <lanbing@baogongyoucai.com>
2018-04-25 16:07:25 +0800

    简单的curd

--- src/main/java/com/bing/lan/mybatis/domain/Employee.java
+++ src/main/java/com/bing/lan/mybatis/domain/Employee.java
@@ -7,17 +7,8 @@ package com.bing.lan.mybatis.domain;
 public class Employee {
 
     private String name;
-    private Long id1;
     private Long id;
     private String phone;
-
-    public Long getId1() {
-        return id1;
-    }
-
-    public void setId1(Long id1) {
-        this.id1 = id1;
-    }
 
     public String getName() {
         return name;
--- src/main/java/com/bing/lan/mybatis/domain/EmployeeTest.java
+++ src/main/java/com/bing/lan/mybatis/domain/EmployeeTest.java
@@ -3,6 +3,8 @@ package com.bing.lan.mybatis.domain;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.Test;
 
+import java.util.List;
+
 /**
  * Created by 蓝兵 on 2018/4/24.
  */
@@ -20,7 +22,63 @@ public class EmployeeTest {
             sqlSession.insert("com.bing.lan.mybatis.domain.EmployeeMapper.save", employee);
 
             sqlSession.commit();
-            sqlSession.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void testUpdate() {
+        Employee employee = new Employee();
+        employee.setId(2L);
+        employee.setName("xiaohong");
+        employee.setPhone("2222123456789");
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtil.openSession();
+            sqlSession.insert("com.bing.lan.mybatis.domain.EmployeeMapper.update", employee);
+            sqlSession.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            if (sqlSession != null) {
+                sqlSession.rollback();
+            }
+        } finally {
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void testGet() {
+        SqlSession sqlSession = MybatisUtil.openSession();
+        Employee employee = sqlSession.selectOne(
+                "com.bing.lan.mybatis.domain.EmployeeMapper.get", 2l);
+    }
+
+    @Test
+    public void testList() {
+        SqlSession sqlSession = MybatisUtil.openSession();
+        List<Employee> employees = sqlSession.selectList(
+                "com.bing.lan.mybatis.domain.EmployeeMapper.list");
+    }
+
+    @Test
+    public void testDelete() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MybatisUtil.openSession();
+            sqlSession.delete(
+                    "com.bing.lan.mybatis.domain.EmployeeMapper.delete", 2l);
+            sqlSession.commit();
         } catch (Exception e) {
             e.printStackTrace();
             if (sqlSession != null) {
